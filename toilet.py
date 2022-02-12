9dee2336f699ebf9aec35b15f6a8f355c4becda3
XOQDY <numfahmaek@gmail.com>
2022-02-12 14:42:55 +0700

    :art: edit PEP8 :art:

--- toilet.py
+++ toilet.py
@@ -38,22 +38,9 @@ class ToiletRoom(BaseModel):
 def calculate_estimate(estimate, spend_time):
     time = estimate["time"]
     quantity = estimate["quantity"]
-    if time == 0:
-        result = spend_time
-    else:
-        all_time = time * quantity
-        result = (all_time + spend_time) / (quantity + 1)
+    all_time = time * quantity
+    result = (all_time + spend_time) / (quantity + 1)
     return result
-
-
-@app.get("/toilet/by-no/{no}")
-def get_status(no: int):
-    num = no
-    query = {"room": num}
-    search = toilet_collection.find_one(query, {"_id": 0})
-    return {
-        "result": search
-    }
 
 
 @app.post("/open-close")
@@ -65,16 +52,17 @@ def add_toilet_open_close(status: ToiletOpen):
             "message": f"Error no room number {room}"
         })
     if status.close:
-        toilet_collection.update_one({"room_number": room}, {"$set": {"close": 1, "time_in": datetime.now().timestamp()}})
+        toilet_collection.update_one({"room_number": room},
+                                     {"$set": {"close": 1, "time_in": datetime.now().timestamp()}})
         return {
             "message": f"Now toilet room {room} in use."
         }
     else:
         toilet_collection.update_one({"room_number": room}, {"$set": {"close": 0}})
         delta = datetime.now().timestamp() - query["time_in"]
-        estimate_list = toilet_collection.find_one({"estimate": "estimate"}, {"_id": 0})
-        estimate_time = calculate_estimate(estimate_list, delta)
-        quantity = estimate_list["quantity"] + 1
+        estimate = toilet_collection.find_one({"estimate": "estimate"}, {"_id": 0})
+        estimate_time = calculate_estimate(estimate, delta)
+        quantity = estimate["quantity"] + 1
         toilet_collection.update_one({"estimate": "estimate"}, {"$set": {"time": estimate_time, "quantity": quantity}})
         return {
             "message": f"Congrats now toilet room {room} is free."
